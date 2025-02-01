5ec0280a07f482a66e66a4b7db849d1252e817a6
Rahulsaini01-wpr <rahul.sainy01@gmail.com>
2025-02-01 10:28:51 +0530

    commit form dev

---
+++ wipro_javaee_project/src/com/wipro/java/HelloWorld.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.wipro.java;
+
+/**
+ * 
+ */
+public class HelloWorld {
+
+	/**
+	 * 
+	 */
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is My first JavaEE Project");
+
+	}
+
+}
